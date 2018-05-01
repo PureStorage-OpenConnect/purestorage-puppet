ab671e8a2bb3f2dac1c41b691e313149061c1dbe
Gavin Williams <fatmcgav@gmail.com>
2018-05-01 16:18:09 +0100

    Improve fact gathering, and fix failing tests

--- lib/puppet/util/network_device/pure/facts.rb
+++ lib/puppet/util/network_device/pure/facts.rb
@@ -2,23 +2,25 @@ require 'puppet/util/network_device/pure'
 require 'puppet/purestorage_api'
 
 class Puppet::Util::NetworkDevice::Pure::Facts
+  attr_reader :transport, :facts
 
-  attr_reader :transport
-  def initialize(trasport)
+  def initialize(transport)
     @transport = transport
-    Puppet.debug("Inside Initialize of Facts!")
   end
 
   def retrieve
-    Puppet.debug("Retrieving Facts from fact.rb!")
-    Puppet.debug("Transport = #{@transport.inspect}")
+    Puppet.debug("Fetching facts from Pure Array")
     array_info = @transport.getRestCall('/array')
-    Puppet.debug("Array_info = #{array_info.inspect}")
+    Puppet.debug("Returned array info = #{array_info.inspect}")
+
+    controller_info = @transport.getRestCall('/array?controllers=true')
+    Puppet.debug("Returned controller info =#{controller_info.inspect}")
 
     @facts = {}
-    @facts['array_name'] = array_info['array_name']
-    @facts['vendor_id'] = 'pure'
-    @facts['version'] = array_info['version']
+    @facts['array_name']  = array_info[:array_name]
+    @facts['controllers'] = controller_info
+    @facts['vendor_id']   = 'pure'
+    @facts['version']     = array_info[:version]
     @facts
   end
 end
--- spec/unit/puppet/util/network_device/pure/facts_spec.rb
+++ spec/unit/puppet/util/network_device/pure/facts_spec.rb
@@ -13,10 +13,59 @@ describe Puppet::Util::NetworkDevice::Pure::Facts do
     }
   end
 
-  it "should get facts from array" do
-    @transport = double(:pure_array)
-    expect(@transport).to receive(:getRestCall).with('/array') {array_details}
-    facts = Puppet::Util::NetworkDevice::Pure::Facts.new(@transport).retrieve
-    expect(facts['vendor_id']).to eq('pure')
+  let(:controller_details) do
+    [
+      {
+        "status": "ready",
+        "model": "FA-m10r2",
+        "version": "4.10.9",
+        "name": "CT0",
+        "mode": "secondary"
+      },
+      {
+        "status": "ready",
+        "model": "FA-m10r2",
+        "version": "4.10.9",
+        "name": "CT1",
+        "mode": "primary"
+      }
+    ]
+  end
+
+  before(:each) do
+    @transport = double('transport')
+    @facts = Puppet::Util::NetworkDevice::Pure::Facts.new(@transport)
+  end
+
+  it 'should have facts read-only' do
+    expect(@facts.facts).to be_nil
+    expect { @facts.facts = 'some new facts' }.to raise_error NoMethodError
+  end
+
+  it 'should have transport read-only' do
+    expect(@facts.transport).to be @transport
+    expect { @facts.transport = 'new transport' }.to raise_error NoMethodError
+  end
+
+  context '#initialize' do
+    it 'should set transport readable' do
+      transport = double('transport')
+      facts = Puppet::Util::NetworkDevice::Pure::Facts.new(transport)
+      expect(facts.transport).to be(transport)
+    end
+  end
+
+  context '#retrieve' do
+    it "should get facts from array" do
+      allow(@transport).to receive(:getRestCall).with('/array').and_return(array_details)
+      allow(@transport).to receive(:getRestCall).with('/array?controllers=true').and_return(controller_details)
+      facts = @facts.retrieve
+      expect(facts).to eq({
+        'array_name'  => 'Pure01',
+        'controllers' => controller_details,
+        'vendor_id'   => 'pure',
+        'version'     => '4.10.9'
+      })
+    end
   end
 end
