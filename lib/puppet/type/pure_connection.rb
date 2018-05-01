7c473d2dd40d4a5ae9f58e60446bdb4fe412d5d2
Gavin Williams <fatmcgav@gmail.com>
2018-05-01 17:30:49 +0100

    Update pure_connection type to use composite namevars for host_name and volume_name params. Add autorequires for pure_host and pure_volume resources. Add unit tests for type.

--- lib/puppet/type/pure_connection.rb
+++ lib/puppet/type/pure_connection.rb
@@ -2,7 +2,7 @@ Puppet::Type.newtype(:pure_connection) do
   @doc = "It does CRUD operations for Host-volume Connection on a Pure Storage flash array."
 
   validate do
-    self.fail "host name and volume name are mandatory" if !self[:host_name] || !self[:volume_name]
+    raise ArgumentError, "host name and volume name are mandatory" if !self[:host_name] || !self[:volume_name]
   end
 
   apply_to_all
@@ -11,19 +11,42 @@ Puppet::Type.newtype(:pure_connection) do
   newparam(:host_name) do
     desc "The name of the host. "
     isnamevar
-    validate do |value|
-      fail("host name: #{value} can not be empty or null") if value == "null" or value.strip.empty?
+
+    munge do |v|
+      v.to_s.strip
     end
   end
        
   newparam(:volume_name) do
     desc "The name of the volume."
-    validate do |value|
-      fail("volume name: #{value} can not be empty or null") if value == "null" or value.strip.empty?
+    isnamevar
+
+    munge do |v|
+      v.to_s.strip
     end
   end
 
   newparam(:device_url) do
     desc "URL in the form of https://<user>:<passwd>@<FQ_Device_Name or IP>"
+  end
+
+  # Support host_name:volume_name format
+  def self.title_patterns
+    [
+      [ %r{(^([^:]*)$)}m,
+        [ [:host_name] ] ],
+      [ %r{^(.*):(.*)$},
+        [ [:host_name], [:volume_name] ]
+      ]
+    ]
+  end
+
+  # Require pure_host and pure_volume resources
+  autorequire(:pure_host) do
+    self[:host_name]
+  end
+
+  autorequire(:pure_volume) do
+    self[:volume_name]
   end
 end
