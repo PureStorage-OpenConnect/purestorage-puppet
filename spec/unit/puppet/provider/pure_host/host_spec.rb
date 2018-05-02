fe6b16e0c713f659b364a962354443d5411f4f18
Gavin Williams <fatmcgav@gmail.com>
2018-05-02 10:58:16 +0100

    Fix indentation

--- spec/unit/puppet/provider/pure_host/host_spec.rb
+++ spec/unit/puppet/provider/pure_host/host_spec.rb
@@ -57,55 +57,55 @@ describe Puppet::Type.type(:pure_host).provider(:host) do
         }
       ])
     end
+  end
 
-    describe '#prefetch' do
-      it 'exists' do
-        expect(@transport).to receive(:getRestCall).with('/host') { JSON.parse(File.read(my_fixture('host-list.json'))) }
-        allow(described_class).to receive(:transport) { @transport }
-        current_provider = resource.provider
-        resources = { 'volume-name' => resource }
-        described_class.prefetch(resources)
-        expect(resources['volume-name']).not_to be(current_provider)
-      end
+  describe '#prefetch' do
+    it 'exists' do
+      expect(@transport).to receive(:getRestCall).with('/host') { JSON.parse(File.read(my_fixture('host-list.json'))) }
+      allow(described_class).to receive(:transport) { @transport }
+      current_provider = resource.provider
+      resources = { 'volume-name' => resource }
+      described_class.prefetch(resources)
+      expect(resources['volume-name']).not_to be(current_provider)
     end
+  end
 
-    describe 'when creating a volume' do
-      it 'should be able to create it' do
-        expect(@transport).to receive(:executeHostRestApi).with('create', 'pure_host', nil, nil)
-        allow(resource.provider).to receive(:transport) { @transport }
-        resource.provider.create
-      end
+  describe 'when creating a volume' do
+    it 'should be able to create it' do
+      expect(@transport).to receive(:executeHostRestApi).with('create', 'pure_host', nil, nil)
+      allow(resource.provider).to receive(:transport) { @transport }
+      resource.provider.create
     end
+  end
 
-    describe 'when destroying a volume' do
-      it 'should be able to delete it' do
-        expect(@transport).to receive(:executeHostRestApi).with('delete', 'pure_host')
+  describe 'when destroying a volume' do
+    it 'should be able to delete it' do
+      expect(@transport).to receive(:executeHostRestApi).with('delete', 'pure_host')
+      allow(resource.provider).to receive(:transport) { @transport }
+      resource.provider.set(:name => 'pure_host')
+      resource.provider.destroy
+      resource.provider.flush
+    end
+  end
+
+  describe 'when modifying a volume' do
+    describe 'for iqnlist' do
+      it "should be able to update iqnlist" do
+        expect(@transport).to receive(:executeHostRestApi).with('update', 'pure_host', ['123456'], nil)
         allow(resource.provider).to receive(:transport) { @transport }
-        resource.provider.set(:name => 'pure_host')
-        resource.provider.destroy
+        # resource.provider.set(:name => 'pure_vol', :size => '10G')
+        resource[:iqnlist] = ['123456']
         resource.provider.flush
       end
     end
 
-    describe 'when modifying a volume' do
-      describe 'for iqnlist' do
-        it "should be able to update iqnlist" do
-          expect(@transport).to receive(:executeHostRestApi).with('update', 'pure_host', ['123456'], nil)
-          allow(resource.provider).to receive(:transport) { @transport }
-          # resource.provider.set(:name => 'pure_vol', :size => '10G')
-          resource[:iqnlist] = ['123456']
-          resource.provider.flush
-        end
-      end
-
-      describe 'for wwnlist' do
-        it "should be able to update wwnlist" do
-          expect(@transport).to receive(:executeHostRestApi).with('update', 'pure_host', nil, ['abcdef'])
-          allow(resource.provider).to receive(:transport) { @transport }
-          # resource.provider.set(:name => 'pure_vol', :size => '10G')
-          resource[:wwnlist] = ['abcdef']
-          resource.provider.flush
-        end
+    describe 'for wwnlist' do
+      it "should be able to update wwnlist" do
+        expect(@transport).to receive(:executeHostRestApi).with('update', 'pure_host', nil, ['abcdef'])
+        allow(resource.provider).to receive(:transport) { @transport }
+        # resource.provider.set(:name => 'pure_vol', :size => '10G')
+        resource[:wwnlist] = ['abcdef']
+        resource.provider.flush
       end
     end
   end
--- spec/unit/puppet/provider/pure_volume/volume_spec.rb
+++ spec/unit/puppet/provider/pure_volume/volume_spec.rb
@@ -61,45 +61,45 @@ describe Puppet::Type.type(:pure_volume).provider(:volume) do
         }
       ])
     end
+  end
 
-    describe '#prefetch' do
-      it 'exists' do
-        expect(@transport).to receive(:getRestCall).with('/volume') { JSON.parse(File.read(my_fixture('volume-list.json'))) }
-        allow(described_class).to receive(:transport) { @transport }
-        current_provider = resource.provider
-        resources = { 'volume-name' => resource }
-        described_class.prefetch(resources)
-        expect(resources['volume-name']).not_to be(current_provider)
-      end
+  describe '#prefetch' do
+    it 'exists' do
+      expect(@transport).to receive(:getRestCall).with('/volume') { JSON.parse(File.read(my_fixture('volume-list.json'))) }
+      allow(described_class).to receive(:transport) { @transport }
+      current_provider = resource.provider
+      resources = { 'volume-name' => resource }
+      described_class.prefetch(resources)
+      expect(resources['volume-name']).not_to be(current_provider)
     end
+  end
 
-    describe 'when creating a volume' do
-      it 'should be able to create it' do
-        expect(@transport).to receive(:executeVolumeRestApi).with('create', 'pure_vol', '10G')
+  describe 'when creating a volume' do
+    it 'should be able to create it' do
+      expect(@transport).to receive(:executeVolumeRestApi).with('create', 'pure_vol', '10G')
+      allow(resource.provider).to receive(:transport) { @transport }
+      resource.provider.create
+    end
+  end
+
+  describe 'when destroying a volume' do
+    it 'should be able to delete it' do
+      expect(@transport).to receive(:executeVolumeRestApi).with('delete', 'pure_vol')
+      allow(resource.provider).to receive(:transport) { @transport }
+      resource.provider.set(:name => 'pure_vol')
+      resource.provider.destroy
+      resource.provider.flush
+    end
+  end
+
+  describe 'when modifying a volume' do
+    describe 'for #size=' do
+      it "should be able to increase a volume size" do
+        expect(@transport).to receive(:executeVolumeRestApi).with('update', 'pure_vol', '20G')
         allow(resource.provider).to receive(:transport) { @transport }
-        resource.provider.create
-      end
-    end
-
-    describe 'when destroying a volume' do
-      it 'should be able to delete it' do
-        expect(@transport).to receive(:executeVolumeRestApi).with('delete', 'pure_vol')
-        allow(resource.provider).to receive(:transport) { @transport }
-        resource.provider.set(:name => 'pure_vol')
-        resource.provider.destroy
-        resource.provider.flush
-      end
-    end
-
-    describe 'when modifying a volume' do
-      describe 'for #size=' do
-        it "should be able to increase a volume size" do
-          expect(@transport).to receive(:executeVolumeRestApi).with('update', 'pure_vol', '20G')
-          allow(resource.provider).to receive(:transport) { @transport }
-          # resource.provider.set(:name => 'pure_vol', :size => '10G')
-          resource[:size] = '20G'
-          resource.provider.send("size=", '20G')
-        end
+        # resource.provider.set(:name => 'pure_vol', :size => '10G')
+        resource[:size] = '20G'
+        resource.provider.send("size=", '20G')
       end
     end
   end
